b9b73284bafba0d96f17d6423e8f98fa36f52832
kamal Jyotwal <kkjyotwal100@gmail.com>
2020-12-07 18:57:19 +0530

    layout ready
    The layout is mostly done, few tweks are needed along with code refactoring which will be done in the end.
    next-commit: logic & tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import './CSS/app.css';
+import "./CSS/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="calculator-body">
-        <div className="screen"></div>
+import React, { Component } from "react";
+
+const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
+const calcoperations = ["AC", "C", "%"];
+const operators = ["÷", "×", "-", "+", "="];
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      screenData: "",
+    };
+  }
+
+  render() {
+    let mappingNumVar = numberArray.map((i) => <div className="button numbers">{i}</div>);
+    let calcOperationsVar = calcoperations.map((i) => <div className="button">{i}</div>);
+    let operatorsVar = operators.map((i) => <div className="operators button">{i}</div>);
+    return (
+      <div className="App">
+        <div className="calculator-body">
+          <div className="screen"></div>
+
+          <div className="buttonContainer">
+            <div className="leftPanel">
+              <div className="calcoperations">{calcOperationsVar}</div>
+              <div className="numerals">{mappingNumVar}</div>
+            </div>
+
+            <div className="operators">{operatorsVar}</div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
